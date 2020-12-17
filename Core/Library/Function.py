85a3d569d3378e6835faca69e4bc453cdf71f23e
defaulterrr <thedefaulterrr@gmail.com>
2020-12-17 08:39:04 +0300

    Added function node class

---
+++ Core/Library/Function.py
@@ -0,0 +1,13 @@
+class Function():
+    def __init__(self, name, resultAddress):
+        self.name = name
+        self.resultAddress = resultAddress
+
+
+    # evaluate addresses, check for previous definitions of a function and add a function to func namespace for calling
+    def eval():
+        pass
+
+    # generate function body to be later added to the end of the file for calling
+    def generate():
+        pass
