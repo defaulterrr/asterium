faea43e3ae97dcc3d36a9babf7fe9d24819a6c7f
defaulterrr <thedefaulterrr@gmail.com>
2020-12-17 08:27:02 +0300

    Reworked compiler mechanics to work with multiline
    Now splits the input by semicolons and works the parts by themselves

--- Core/Compiler.py
+++ Core/Compiler.py
@@ -2,19 +2,45 @@ from Lexer import Lexer
 from Parser import Parser
 from Library.AddressTable import AddressTable
 
-input = '1/0*999-521456*83/7'
+raw_input = """
+var variable;
+variable = 127;
+var variable2;
+variable2 = 127 * 8;
+"""
 
+input = raw_input.split(";")
+sorted_expressions = []
+for index in range(len(input)):
+    if input[index] != "\n":
+        sorted_expressions.append(input[index]+";")
+print(input)
+    
 table = AddressTable()
 
 lexer = Lexer().get_lexer()
-tokens = lexer.lex(input)
-pg = Parser()
-pg.parse()
-parser = pg.getParser()
+outputs = []
 
-tree = parser.parse(tokens)
-tree.eval(table)
+for index in range(len(sorted_expressions)):
+    expression = sorted_expressions[index]
+    print("Working on: " + expression)
+    tokens = lexer.lex(expression)
+
+    pg = Parser(debug=True)
+    pg.parse()
+    parser = pg.getParser()
+    # table.print()
+    tree = parser.parse(tokens)
+    tree.eval(table)
+    # outputs.append(tree.generate(table))
+    output = tree.generate(table)
+    if output != None:
+        outputs.append(output)
+    print("Worked out expression: " + expression)
+print("RESULT")
 print("main: start 0")
+for output in outputs:
+    print(output)
 
-print(tree.generate(table))
+print("_____________")
 print(table.generate())
